59ca91de34d42676747ebc28c0cfd6a59aa92448
Scott Romer <scott.romer@gmail.com>
2023-09-12 18:15:27 -0400

    Updating logo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import logo from './logo.svg';
+import React from 'react';
+import logo from './decatur_logo.png';
 import './App.css';
 
 function App() {
@@ -7,16 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Woof woof!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
