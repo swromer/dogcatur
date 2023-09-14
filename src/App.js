f264fcc438ed5960b09f9589a370e46d3396a3b3
Scott Romer <scott.romer@gmail.com>
2023-09-14 19:46:53 -0400

    Debugging cognito

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React, { useEffect, useState } from 'react'
 import logo from './dogcaturga_logo.png';
-import './App.css';
 
-function App() {
+import { Amplify, API, graphqlOperation } from 'aws-amplify';
+import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
+
+import './App.css';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsConfig from './aws-exports';
+
+//import { createUser, updateUser, deleteUser } from './graphql/mutations'
+//import { listUsers } from './graphql/queries'
+
+Amplify.configure(awsConfig);
+
+/*const result = await API.graphql(graphqlOperation(createUser, {
+  input: {
+    name: 'Scott Romer'
+  }
+}))
+
+const usersResult = await API.graphql(graphqlOperation(listUsers))
+console.log(result)
+*/
+
+function App({signOut, user}) {
   return (
     <div className="App">
-        <Heading level={1}>Hello dude!</Heading>
-        <Button>Sign out</Button>
+        <Heading level={1}>Hello {user.username}</Heading>
+        <Button onClick={signOut}>Sign out</Button>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
@@ -17,4 +39,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App);
